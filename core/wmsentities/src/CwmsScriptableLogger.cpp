9418a51442194bcc240f936126474d24221aeb74
wgrasshof <wg@wogra.com>
2021-02-24 13:16:32 +0100

    removed old classwizard headers.

--- core/wmsentities/src/CwmsScriptableLogger.cpp
+++ core/wmsentities/src/CwmsScriptableLogger.cpp
@@ -23,63 +23,34 @@
 #include "CwmsScriptableLogger.h"
 
 
-/** +-=---------------------------------------------------------Mi 9. Okt 14:39:10 2013-----------*
- * @method  CwmsScriptableLogger::CwmsScriptableLogger       // public                            *
- * @return                                                   //                                   *
- * @comment                                                                                       *
- *----------------last changed: --------------------------------Mi 9. Okt 14:39:10 2013-----------*/
 CwmsScriptableLogger::CwmsScriptableLogger()
 {
 }
 
-/** +-=---------------------------------------------------------Mi 9. Okt 14:40:18 2013-----------*
- * @method  CwmsScriptableLogger::~CwmsScriptableLogger      // public, virtual                   *
- * @return  void                                             //                                   *
- * @comment The Destructor of Class CwmsScriptableLogger                                          *
- *----------------last changed: --------------------------------Mi 9. Okt 14:40:18 2013-----------*/
+
 CwmsScriptableLogger::~CwmsScriptableLogger()
 {
 }
 
-/** +-=---------------------------------------------------------Mi 9. Okt 14:44:45 2013-----------*
- * @method  CwmsScriptableLogger::Info                       // public, slots                     *
- * @return  void                                             //                                   *
- * @param   QString p_qstrMessage                            //                                   *
- * @comment                                                                                       *
- *----------------last changed: --------------------------------Mi 9. Okt 14:44:45 2013-----------*/
+
 void CwmsScriptableLogger::info(QString p_qstrMessage)
 {
    CdmLogging::AddInfoLog("Javascript Output", p_qstrMessage, 0);
 }
 
-/** +-=---------------------------------------------------------Mi 9. Okt 14:44:31 2013-----------*
- * @method  CwmsScriptableLogger::Warning                    // public, slots                     *
- * @return  void                                             //                                   *
- * @param   QString p_qstrMessage                            //                                   *
- * @comment                                                                                       *
- *----------------last changed: --------------------------------Mi 9. Okt 14:44:31 2013-----------*/
+
 void CwmsScriptableLogger::warning(QString p_qstrMessage)
 {
    CdmLogging::AddWarningLog("Javascript Output", p_qstrMessage, 0);
 }
 
-/** +-=---------------------------------------------------------Mi 9. Okt 14:44:22 2013-----------*
- * @method  CwmsScriptableLogger::Error                      // public, slots                     *
- * @return  void                                             //                                   *
- * @param   QString p_qstrMessage                            //                                   *
- * @comment                                                                                       *
- *----------------last changed: --------------------------------Mi 9. Okt 14:44:22 2013-----------*/
+
 void CwmsScriptableLogger::error(QString p_qstrMessage)
 {
    CdmLogging::AddErrorLog("Javascript Output", p_qstrMessage, 0);
 }
 
-/** +-=---------------------------------------------------------Mi 9. Okt 14:45:28 2013-----------*
- * @method  CwmsScriptableLogger::Fatal                      // public, slots                     *
- * @return  void                                             //                                   *
- * @param   QString p_qstrMessage                            //                                   *
- * @comment                                                                                       *
- *----------------last changed: --------------------------------Mi 9. Okt 14:45:28 2013-----------*/
+
 void CwmsScriptableLogger::fatal(QString p_qstrMessage)
 {
    CdmLogging::AddFatalLog("Javascript Output", p_qstrMessage, 0);
--- core/wmsentities/src/CwmsScriptableLogger.h
+++ core/wmsentities/src/CwmsScriptableLogger.h
@@ -3,13 +3,13 @@
  ** Modulename: CwmsScriptableLogger.h
  ** Started Implementation: 2012/11/09
  ** Description:
- ** 
+ **
  ** implements the script interface for js methods using logging
  **
  ** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
- ** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. 
+ ** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  **(C) copyright by WOGRA technologies GmbH & Co KG All rights reserved
- *****************************************************************************/ 
+ *****************************************************************************/
 
 #ifndef CWMSSCRIPTABLELOGGER_H
 #define CWMSSCRIPTABLELOGGER_H
@@ -39,58 +39,15 @@ class BASETOOLS_API CwmsScriptableLogger: public CwmsScriptableBase
 {
     Q_OBJECT
 
-
-   public:
-   /** +-=---------------------------------------------------------Mi 9. Okt 14:39:10 2013--------*
-    * @method  CwmsScriptableLogger::CwmsScriptableLogger    // public                            *
-    * @return                                                //                                   *
-    * @comment                                                                                    *
-    *----------------last changed: -----------------------------Mi 9. Okt 14:39:10 2013-----------*/
+public:
     CwmsScriptableLogger( );
-
-   public:
-   /** +-=---------------------------------------------------------Mi 9. Okt 14:40:18 2013--------*
-    * @method  CwmsScriptableLogger::~CwmsScriptableLogger   // public, virtual                   *
-    * @return  void                                          //                                   *
-    * @comment The Destructor of Class CwmsScriptableLogger                                       *
-    *----------------last changed: -----------------------------Mi 9. Okt 14:40:18 2013-----------*/
     virtual ~CwmsScriptableLogger( );
 
-    public slots:
-         /** +-=---------------------------------------------------------Mi 9. Okt 14:44:45 2013--------*
-    * @method  CwmsScriptableLogger::Info                    // public, slots                     *
-    * @return  void                                          //                                   *
-    * @param   QString p_qstrMessage                         //                                   *
-    * @comment                                                                                    *
-    *----------------last changed: -----------------------------Mi 9. Okt 14:44:45 2013-----------*/
-void info( QString p_qstrMessage);
-
-    public slots:
-         /** +-=---------------------------------------------------------Mi 9. Okt 14:44:31 2013--------*
-    * @method  CwmsScriptableLogger::Warning                 // public, slots                     *
-    * @return  void                                          //                                   *
-    * @param   QString p_qstrMessage                         //                                   *
-    * @comment                                                                                    *
-    *----------------last changed: -----------------------------Mi 9. Okt 14:44:31 2013-----------*/
-void warning( QString p_qstrMessage);
-
-    public slots:
-         /** +-=---------------------------------------------------------Mi 9. Okt 14:44:22 2013--------*
-    * @method  CwmsScriptableLogger::Error                   // public, slots                     *
-    * @return  void                                          //                                   *
-    * @param   QString p_qstrMessage                         //                                   *
-    * @comment                                                                                    *
-    *----------------last changed: -----------------------------Mi 9. Okt 14:44:22 2013-----------*/
-void error( QString p_qstrMessage);
-
-    public slots:
-         /** +-=---------------------------------------------------------Mi 9. Okt 14:45:28 2013--------*
-    * @method  CwmsScriptableLogger::Fatal                   // public, slots                     *
-    * @return  void                                          //                                   *
-    * @param   QString p_qstrMessage                         //                                   *
-    * @comment                                                                                    *
-    *----------------last changed: -----------------------------Mi 9. Okt 14:45:28 2013-----------*/
-void fatal( QString p_qstrMessage);
+public slots:
+    void info( QString p_qstrMessage);
+    void warning( QString p_qstrMessage);
+    void error( QString p_qstrMessage);
+    void fatal( QString p_qstrMessage);
 };
 
 #endif //
