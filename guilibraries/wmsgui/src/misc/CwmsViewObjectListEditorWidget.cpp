4c1518f2c8bdfdd7e19fb3fd340d9fce51d6fa0d
wgrasshof <wg@wogra.com>
2021-08-23 16:58:09 +0200

    removed unnecessary headers.

--- guilibraries/wmsgui/src/misc/CwmsViewObjectListEditorWidget.cpp
+++ guilibraries/wmsgui/src/misc/CwmsViewObjectListEditorWidget.cpp
@@ -1,17 +1,4 @@
-﻿/******************************************************************************
- ** WOGRA technologies GmbH & Co. KG Modul Information
- ** Modulename: CwmsViewObjectListEditorWidget.cpp
- ** Started Implementation: 2012/09/09
- ** Description:
- **
- ** implements an objectlisteditor with different views for user selection
- **
- ** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
- ** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. 
- **(C) copyright by WOGRA technologies GmbH & Co. KG All rights reserved
- *****************************************************************************/
-
-// System and QT Includes
+﻿// System and QT Includes
 
 
 // own Includes
@@ -22,44 +9,22 @@
 #include "CwmsViewObjectListEditorWidget.h"
 
 
-/** +-=---------------------------------------------------------So 9. Sep 12:30:27 2012-----------*
- * @method  CwmsViewObjectListEditorWidget::CwmsViewObjectListEditorWidget // public              *
- * @return                                                   //                                   *
- * @param   QWidget* p_pqwParent                             //                                   *
- * @comment                                                                                       *
- *----------------last changed: --------------------------------So 9. Sep 12:30:27 2012-----------*/
 CwmsViewObjectListEditorWidget::CwmsViewObjectListEditorWidget(QWidget* p_pqwParent)
 : QWidget(p_pqwParent)
 {
    setupUi(this);
 }
 
-/** +-=---------------------------------------------------------So 9. Sep 12:31:03 2012-----------*
- * @method  CwmsViewObjectListEditorWidget::~CwmsViewObjectListEditorWidget // public, virtual    *
- * @return  void                                             //                                   *
- * @comment The Destructor of Class CwmsViewObjectListEditorWidget                                *
- *----------------last changed: --------------------------------So 9. Sep 12:31:03 2012-----------*/
 CwmsViewObjectListEditorWidget::~CwmsViewObjectListEditorWidget()
 {
 }
 
-/** +-=---------------------------------------------------------So 9. Sep 12:31:11 2012-----------*
- * @method  CwmsViewObjectListEditorWidget::FillDialog       // private                           *
- * @return  void                                             //                                   *
- * @comment                                                                                       *
- *----------------last changed: --------------------------------So 9. Sep 12:31:11 2012-----------*/
 void CwmsViewObjectListEditorWidget::FillDialog()
 {
    m_pCwmsEditor->FillDialog();
    ViewSelectionChangedSlot();
 }
 
-/** +-=---------------------------------------------------------Fr 14. Sep 10:39:14 2012----------*
- * @method  CwmsViewObjectListEditorWidget::AddView          // public                            *
- * @return  void                                             //                                   *
- * @param   QString p_qstrViewName                           //                                   *
- * @comment                                                                                       *
- *----------------last changed: --------------------------------Fr 14. Sep 10:39:14 2012----------*/
 void CwmsViewObjectListEditorWidget::AddView(QString p_qstrViewName)
 {
    QListWidgetItem* pItem = new QListWidgetItem(m_pqlwFilters);
@@ -71,12 +36,6 @@ void CwmsViewObjectListEditorWidget::AddView(QString p_qstrViewName)
    }
 }
 
-/** +-=---------------------------------------------------------So 9. Sep 12:32:19 2012-----------*
- * @method  CwmsViewObjectListEditorWidget::LoadView         // private                           *
- * @return  CwmsView                                         //                                   *
- * @param   QString p_qstrName                               //                                   *
- * @comment                                                                                       *
- *----------------last changed: --------------------------------So 9. Sep 12:32:19 2012-----------*/
 CwmsView CwmsViewObjectListEditorWidget::LoadView(QString p_qstrName)
 {
    CwmsViewManager cManager;
@@ -84,11 +43,6 @@ CwmsView CwmsViewObjectListEditorWidget::LoadView(QString p_qstrName)
    return cView;
 }
 
-/** +-=---------------------------------------------------------So 9. Sep 12:36:30 2012-----------*
- * @method  CwmsViewObjectListEditorWidget::ViewSelectionChangedSlot // private, slots            *
- * @return  void                                             //                                   *
- * @comment                                                                                       *
- *----------------last changed: --------------------------------So 9. Sep 12:36:30 2012-----------*/
 void CwmsViewObjectListEditorWidget::ViewSelectionChangedSlot()
 {
    QListWidgetItem* pItem = GetSelectedView();
@@ -102,11 +56,6 @@ void CwmsViewObjectListEditorWidget::ViewSelectionChangedSlot()
    }
 }
 
-/** +-=---------------------------------------------------------So 9. Sep 12:40:34 2012-----------*
- * @method  CwmsViewObjectListEditorWidget::GetSelectedView  // public                            *
- * @return  QListWidgetItem*                                 //                                   *
- * @comment                                                                                       *
- *----------------last changed: --------------------------------So 9. Sep 12:40:34 2012-----------*/
 QListWidgetItem* CwmsViewObjectListEditorWidget::GetSelectedView()
 {
    QListWidgetItem* pItem = nullptr;
@@ -120,25 +69,12 @@ QListWidgetItem* CwmsViewObjectListEditorWidget::GetSelectedView()
    return pItem;
 }
 
-/** +-=---------------------------------------------------------Di 23. Jul 13:21:01 2013----------*
- * @method  CwmsViewObjectListEditorWidget::ConnectRuntime   // private                           *
- * @return  void                                             //                                   *
- * @param   CwmsRuntime* p_pRuntime                          //                                   *
- * @param   CwmsApplicationModule p_cModule                  //                                   *
- * @comment                                                                                       *
- *----------------last changed: --------------------------------Di 23. Jul 13:21:01 2013----------*/
 void CwmsViewObjectListEditorWidget::ConnectRuntime(CwmsRuntime* p_pRuntime,
                                                     CwmsApplicationModule p_cModule)
 {
   m_pCwmsEditor->ConnectRuntime(p_pRuntime, p_cModule);
 }
 
-/** +-=---------------------------------------------------------Mo 17. Sep 10:36:56 2012----------*
- * @method  CwmsViewObjectListEditorWidget::SetModule        // private                           *
- * @return  void                                             //                                   *
- * @param   CwmsApplicationModule p_cModule                  //                                   *
- * @comment                                                                                       *
- *----------------last changed: --------------------------------Mo 17. Sep 10:36:56 2012----------*/
 void CwmsViewObjectListEditorWidget::SetModule(CwmsApplicationModule p_cModule)
 {
    if (p_cModule.IsValid())
@@ -165,13 +101,6 @@ void CwmsViewObjectListEditorWidget::SetModule(CwmsApplicationModule p_cModule)
    }
 }
 
-/** +-=---------------------------------------------------------Mo 17. Sep 10:36:40 2012----------*
- * @method  CwmsViewObjectListEditorWidget::Initialize       // public                            *
- * @return  void                                             //                                   *
- * @param   CwmsApplicationModule p_cModule                  //                                   *
- * @param   CwmsRuntime* p_pRuntime                          //                                   *
- * @comment                                                                                       *
- *----------------last changed: --------------------------------Mo 17. Sep 10:36:40 2012----------*/
 void CwmsViewObjectListEditorWidget::Initialize(CwmsApplicationModule p_cModule,
                                                 CwmsRuntime* p_pRuntime)
 {
--- guilibraries/wmsgui/src/misc/CwmsViewObjectListEditorWidget.h
+++ guilibraries/wmsgui/src/misc/CwmsViewObjectListEditorWidget.h
@@ -1,17 +1,4 @@
-﻿/******************************************************************************
- ** WOGRA technologies Gmbh & Co KG Modul Information
- ** Modulename: CwmsViewObjectListEditorWidget.h
- ** Started Implementation: 2012/09/09
- ** Description:
- ** 
- ** implements an objectlisteditor with different views for user selection
- **
- ** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
- ** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. 
- **(C) copyright by WOGRA technologies GmbH & Co KG All rights reserved
- *****************************************************************************/ 
-
-#ifndef CWMSVIEWOBJECTLISTEDITORWIDGET_H
+﻿#ifndef CWMSVIEWOBJECTLISTEDITORWIDGET_H
 #define CWMSVIEWOBJECTLISTEDITORWIDGET_H
 
 #ifdef WURZELBLA
@@ -41,72 +28,25 @@ class WMSGUI_API CwmsViewObjectListEditorWidget : public QWidget, public Ui::Cwm
    Q_OBJECT
 
    public:
-   /** +-=---------------------------------------------------------So 9. Sep 12:30:27 2012--------*
-    * @method  CwmsViewObjectListEditorWidget::CwmsViewObjectListEditorWidget // public           *
-    * @return                                                //                                   *
-    * @param   QWidget* p_pqwParent                          //                                   *
-    * @comment                                                                                    *
-    *----------------last changed: -----------------------------So 9. Sep 12:30:27 2012-----------*/
     CwmsViewObjectListEditorWidget( QWidget* p_pqwParent);
-
-   public:
-   /** +-=---------------------------------------------------------So 9. Sep 12:31:03 2012--------*
-    * @method  CwmsViewObjectListEditorWidget::~CwmsViewObjectListEditorWidget // public, virtual *
-    * @return  void                                          //                                   *
-    * @comment The Destructor of Class CwmsViewObjectListEditorWidget                             *
-    *----------------last changed: -----------------------------So 9. Sep 12:31:03 2012-----------*/
     virtual ~CwmsViewObjectListEditorWidget( );
 
    private:
-   /** +-=---------------------------------------------------------So 9. Sep 12:31:11 2012--------*
-    * @method  CwmsViewObjectListEditorWidget::FillDialog    // private                           *
-    * @return  void                                          //                                   *
-    * @comment                                                                                    *
-    *----------------last changed: -----------------------------So 9. Sep 12:31:11 2012-----------*/
     void FillDialog();
 
    public:
-   /** +-=---------------------------------------------------------Fr 14. Sep 10:39:14 2012-------*
-    * @method  CwmsViewObjectListEditorWidget::AddView       // public                            *
-    * @return  void                                          //                                   *
-    * @param   QString p_qstrViewName                        //                                   *
-    * @comment                                                                                    *
-    *----------------last changed: -----------------------------Fr 14. Sep 10:39:14 2012----------*/
     void AddView(QString p_qstrViewName);
 
    private:
-   /** +-=---------------------------------------------------------So 9. Sep 12:32:19 2012--------*
-    * @method  CwmsViewObjectListEditorWidget::LoadView      // private                           *
-    * @return  CwmsView                                      //                                   *
-    * @param   QString p_qstrName                            //                                   *
-    * @comment                                                                                    *
-    *----------------last changed: -----------------------------So 9. Sep 12:32:19 2012-----------*/
     CwmsView LoadView(QString p_qstrName);
 
     private slots:
-   /** +-=---------------------------------------------------------So 9. Sep 12:36:30 2012--------*
-    * @method  CwmsViewObjectListEditorWidget::ViewSelectionChangedSlot // private, slots         *
-    * @return  void                                          //                                   *
-    * @comment                                                                                    *
-    *----------------last changed: -----------------------------So 9. Sep 12:36:30 2012-----------*/
-void ViewSelectionChangedSlot( );
+    void ViewSelectionChangedSlot( );
 
    public:
-   /** +-=---------------------------------------------------------So 9. Sep 12:40:34 2012--------*
-    * @method  CwmsViewObjectListEditorWidget::GetSelectedView // public                          *
-    * @return  QListWidgetItem*                              //                                   *
-    * @comment                                                                                    *
-    *----------------last changed: -----------------------------So 9. Sep 12:40:34 2012-----------*/
     QListWidgetItem* GetSelectedView();
 
    private:
-   /** +-=---------------------------------------------------------Di 23. Jul 13:21:01 2013-------*
-    * @method  CwmsViewObjectListEditorWidget::ConnectRuntime // private                          *
-    * @return  void                                          //                                   *
-    * @param   CwmsRuntime* p_pRuntime                       //                                   *
-    * @param   CwmsApplicationModule p_cModule               //                                   *
-    * @comment                                                                                    *
-    *----------------last changed: -----------------------------Di 23. Jul 13:21:01 2013----------*/
     void ConnectRuntime(CwmsRuntime* p_pRuntime, CwmsApplicationModule p_cModule);
 
    private:
