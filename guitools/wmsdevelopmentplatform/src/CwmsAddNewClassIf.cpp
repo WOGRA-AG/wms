bd1eec9a9bec7c55084c08c90b6e2e150b135aad
wgrasshof <wg@wogra.com>
2022-03-17 16:51:45 +0100

    rremoved comments

--- guitools/wmsdevelopmentplatform/src/CwmsAddNewClassIf.cpp
+++ guitools/wmsdevelopmentplatform/src/CwmsAddNewClassIf.cpp
@@ -1,16 +1,3 @@
-﻿/******************************************************************************
- ** WOGRA Solutions Modul Information
- ** Modulename: CwmsAddNewClassIf.cpp
- ** Started Implementation: 2008/05/23
- ** Description:
- ** 
- ** implements the class creation dlg
- **
- ** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
- ** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. 
- **(C) copyright by WOGRA Solutions All rights reserved
- *****************************************************************************/ 
-
 // System and QT Includes
 
 // WMS Includes
@@ -21,42 +8,22 @@
 #include "CwmsAddNewClassIf.h"
 
 
-/** +-=---------------------------------------------------------Mo 27. Aug 18:34:23 2012----------*
- * @method  CwmsAddNewClassIf::CwmsAddNewClassIf             // public                            *
- * @return                                                   //                                   *
- * @param   QWidget* parent = nullptr                           //                                   *
- * @comment                                                                                       *
- *----------------last changed: --------------------------------Mo 27. Aug 18:34:23 2012----------*/
 CwmsAddNewClassIf::CwmsAddNewClassIf(QWidget* parent)
    : QDialog(parent)
 {
    setupUi(this);
 }
 
-/** +-=---------------------------------------------------------Fr 23. Mai 20:29:44 2008----------*
- * @method  CwmsAddNewClassIf::~CwmsAddNewClassIf            // public, virtual                   *
- * @return  void                                             //                                   *
- * @comment The Destructor of Class CwmsAddNewClassIf                                             *
- *----------------last changed: --------------------------------Fr 23. Mai 20:29:44 2008----------*/
 CwmsAddNewClassIf::~CwmsAddNewClassIf()
 {
 }
 
-/** +-=---------------------------------------------------------Fr 23. Mai 20:30:46 2008----------*
- * @method  CwmsAddNewClassIf::CancelClickedSlot             // private, slots                    *
- * @return  void                                             //                                   *
- * @comment                                                                                       *
- *----------------last changed: --------------------------------Fr 23. Mai 20:30:46 2008----------*/
 void CwmsAddNewClassIf::CancelClickedSlot()
 {
    reject();
 }
 
-/** +-=---------------------------------------------------------Do 27. Dez 12:32:55 2012----------*
- * @method  CwmsAddNewClassIf::Validate                      // private                           *
- * @return  bool                                             //                                   *
- * @comment                                                                                       *
- *----------------last changed: --------------------------------Do 27. Dez 12:32:55 2012----------*/
+
 bool CwmsAddNewClassIf::Validate()
 {
    bool bRet = true;
@@ -92,11 +59,6 @@ bool CwmsAddNewClassIf::Validate()
    return bRet;
 }
 
-/** +-=---------------------------------------------------------Fr 23. Mai 20:30:33 2008----------*
- * @method  CwmsAddNewClassIf::OKClickedSlot                 // private, slots                    *
- * @return  void                                             //                                   *
- * @comment                                                                                       *
- *----------------last changed: --------------------------------Fr 23. Mai 20:30:33 2008----------*/
 void CwmsAddNewClassIf::OKClickedSlot()
 {
    if (Validate())
